a9c5d898abad019a03c66ba546ae014a047985f3
Artem <andrushenko@bestweb.ru>
2023-08-31 13:07:16 +0500

    add handleFileSelected

--- js/handleFileSelected.js
+++ js/handleFileSelected.js
@@ -1,11 +1,18 @@
 function handleFileSelected(input) {
+
     let file = input.files[0]
-    console.log(file)
+
     let reader = new FileReader()
     reader.readAsDataURL(file)
     
     reader.onload = function () {
         let wrapper = document.createElement('div')
+
+        wrapper.addEventListener('click', function() {
+            input.value = ""
+            this.remove()
+        })
+
         wrapper.classList.add("preload-wrap");
         let img = document.createElement('img')
         img.src = reader.result  
